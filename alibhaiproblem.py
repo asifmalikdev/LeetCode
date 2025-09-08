6b32bc01f645e50d77e13afee5766d5302dd0c78
unknown <asifhameed8944@gmail.com>
2025-09-08 17:49:33 +0500

    Integer to 2 postive interger without zero

--- alibhaiproblem.py
+++ alibhaiproblem.py
@@ -48,3 +48,72 @@ obj  = solution()
 print("after class object and before function")
 obj.prac()
 print(x)
+
+my_dict = {"a": 1, "b": 2, "c": 3}
+
+keys = my_dict.values()
+print(keys)
+
+print(my_dict["a"])
+print(my_dict.get("d", 99))
+
+my_dict["d"] = 768
+
+print(my_dict)
+
+my_dict = {"a": 1, "b": 2, "c": 3}
+my_dict["some_key"] = None
+print(my_dict)
+
+for key,value in my_dict.items():
+    print(key, value)
+
+
+print(my_dict.pop(1,89))
+
+
+x = [1, 2, 3]
+y = x
+y[0] = 10
+print(id(x), id(y))
+
+y = list(x)
+print(id(x), id(y))
+
+
+def outer_function(x):
+    def inner_function():
+        return x + 1
+    return inner_function
+
+closure = outer_function(5)
+result = closure
+print(result)
+
+
+def mysterious_function(a, b=[]):
+    b.append(a)
+    return b
+
+result1 = mysterious_function(1)
+print(result1)
+result2 = mysterious_function(2)
+print(result2)
+result3 = mysterious_function(3)
+print(result3)
+print(result1)
+print(result1 + result2 + result3)
+
+def power(x, n=2):
+    return x ** n
+
+result2 = power(2, 3)
+print(result2)
+
+
+
+# class CustomError(Exception):
+#     def __init__(self, message):
+#         super().__init__(message)
+
+# raise CustomError("An example custom error.")
