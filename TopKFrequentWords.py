322072fd361ac7be2a0cb09b5cbf871debbeffef
unknown <asifhameed8944@gmail.com>
2025-09-19 06:30:02 +0500

    Tok K Frequent Words

---
+++ TopKFrequentWords.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def topKFrequent(self, words, k):
+        dict1 = {}
+        for word in words:
+            dict1[word] = dict1.get(word, 0) + 1
+        
+        sorted_words = sorted(dict1.items(), key=lambda x: (-x[1], x[0]))
+        res = [item[0] for item in sorted_words[:k]]
+        return res
+
+words = ["the","day","is","sunny","the","the","the","sunny","is","is"]
+k = 4
+obj = Solution()
+print(obj.topKFrequent(words, k))
