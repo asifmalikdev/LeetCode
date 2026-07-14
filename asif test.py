87c983b50287a795521edf92a5c05544899445b4
asifmalikdev <asifhameed8944@gmail.com>
2026-07-14 07:46:19 +0500

    python basic relooping

---
+++ asif test.py
@@ -0,0 +1,18 @@
+x = 5
+y = 5
+#print(id(x))  #140710702044408
+#print(id(y))  #140710702044408
+
+
+
+lis1 = [1,2,3]
+lis2 = [1,2,3]
+lis3 = lis2
+#print(id(lis1))   #2216803613952
+#print(id(lis2))   #2216803497536
+#print(id(lis3))   #2216803497536
+
+
+
+exapmle_dict = dict()
+print(exapmle_dict)
