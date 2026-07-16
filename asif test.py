0d241a35631d5864e5b71e0453c7a43c920f8f3b
asifmalikdev <asifhameed8944@gmail.com>
2026-07-16 07:11:49 +0500

    memory management in python

--- asif test.py
+++ asif test.py
@@ -20,8 +20,57 @@ exapmle_dict = dict()
 
 
 def greet(age, name):
-    print(name, age)
+    print(name, age)  #24 asif
 
 age = 24
 name = "asif"
-greet(name, age)
+#greet(name, age)
+
+x = [1,2,3]
+y = [1,2,3]
+z = x
+
+#print(id(x))  #2979899736960
+#print(id(y))  #2979899737088
+#print(id(z))  #2979899736960
+
+
+x = 5
+y = 6
+#print(id(x))  #140711288395000
+#print(id(y))  #140711288395032
+
+x = "hello"
+y = "hello"
+#print(id(x))  #2139574722000
+#print(id(y))  #2139574722000
+
+
+x = 65
+#print(id(x))  #140711288396920
+x = 54
+#print(id(x))  #140711288396568
+
+x = "hello"
+#print(id(x))  #1460078402880
+x = "june"
+#print(id(x))  #1460078403360
+
+
+x = "hello"
+#x[0] = "h"  #error
+
+x = "hello"
+x = x.upper()
+#print(x)
+
+
+x = 10
+y = x
+#print(id(x))  #140711288395160
+#print(id(y))  #140711288395160
+x = 20
+#print(id(x))  #140711288395480
+#print(id(y))  #140711288395160
+#PROFFING The biggest proof: multiple references
+print('eh')
