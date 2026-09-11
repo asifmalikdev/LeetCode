76aa17496ffef41823b0542e1424880279a0bb68
temp user <tempuser@Rao-Furqan.local>
2026-09-12 01:49:00 +0500

    Partition of Arrya problem

---
+++ PartitionArray.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def pivotArray(self, nums, pivot):
+        n = len(nums)
+        ans = [pivot]*n
+        left = 0
+        right = n-1
+        for i in range(n):
+            if nums[i] < pivot:
+                ans[left] = nums[i]
+                left+=1
+            if nums[n-1-i] > pivot:
+                ans[right] = nums[n-1-i]
+                right-=1
+        return ans
